b8979fbb976f08cd396fffd0b054d478d00e3c06
Marcellin Wabo <marcellin@wabo.work>
2022-03-23 10:38:42 +0100

    Server: Add req url parser

--- index.js
+++ index.js
@@ -5,10 +5,25 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res){
+    
+    // Get the url and parse it
+    // true make url object call the query string 
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path from the url
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+    // Send the response
     res.end('Welcome to jaxin API\n');
+
+    // Log the request path
+    console.log('Request received on path: '+trimmedPath);
+    
 });
 
 // Start the server, and have it listen on port 3000
