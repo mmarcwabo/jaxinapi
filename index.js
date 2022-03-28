19c752f5bf87f5b7f0b8ccb934833758d737d0e5
Marcellin Wabo <marcellin@wabo.work>
2022-03-28 12:54:15 +0100

    Server, Workers, Checks
    * Refactor index.js structure
    * Add workers
    * Add server.js lib

--- index.js
+++ index.js
@@ -3,112 +3,23 @@
  *  
 */
 
-// Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var fs = require('fs');
-var stringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
-// @TODO GET RID OF THIS
-helpers.sendSMSNotification('993803355', 'Hello mate, from jaxinapi - by exsofth', function(err){
-    console.log('This was the error', err);
-});
+// Declare the app
+var app = {};
 
-// Instantiate the http server 
-var httpServer = http.createServer(function (req, res) {
-    unifiedServer(req, res);
-});
+// Init function
+app.init = function(){
+    // Start the server
+    server.init();
 
-var httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem')
-}
-
-// Instantiate the http server
-var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
-    unifiedServer(req, res);
-});
-
-// Start the http server
-httpServer.listen(config.httpPort, function () {
-    console.log("Jaxin is listening on port " + config.httpPort + " in " + config.envName + " now");
-});
-
-// Start the https server
-httpsServer.listen(config.httpsPort, function () {
-    console.log("Jaxin is listening on port " + config.httpsPort + " in " + config.envName + " now");
-});
-
-// Unified server
-var unifiedServer = function (req, res) {
-    // Get the url and parse it
-    // true make url object call the query string The server should respond to all requests with a string
-    var parsedUrl = url.parse(req.url, true);
-
-    // Get the path from the url
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '')
-
-    // Get the query string as an object
-    var queryStringObject = parsedUrl.query;
-
-    // Get the HTTP method
-    var method = req.method.toLowerCase();
-
-    // Get the headers as an onject
-    var headers = req.headers;
-
-    // Get the payload, if any
-    var decoder = new stringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', function (data) {
-        buffer += decoder.write(data);
-    });
-    req.on('end', function () {
-        buffer += decoder.end();
-
-        // Choose the handler request should go to. If
-        var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-        // Construct the data object to send to the handler
-        var data = {
-            'trimmedPath': trimmedPath,
-            'queryString': queryStringObject,
-            'method': method,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer)
-        };
-
-        // Route the request to the handler specified is the router
-        chosenHandler(data, function (statusCode, payload) {
-            // Use the status code called back by the handler
-            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
-
-            // Use the payload
-            payload = typeof (payload) == 'object' ? payload : {}
-
-            // Convert the payload to a string
-            var payloadString = JSON.stringify(payload);
-
-            // Return the response (json)
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-            // Log the request path
-            console.log('Returning this response: ', statusCode, payloadString);
-        });
-
-    });
+    // Start the workers
+    //workers.init();
 };
 
-//Define request router
-var router = {
-    'ping': handlers.ping,
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks
-};
+// Execute the app
+app.init();
+
+// Export the app
+module.exports = app;
