bb19e6c7830efea6bd578c75b126c65fd7ff793b
Marcellin Wabo <marcellin@wabo.work>
2022-04-05 13:53:55 +0100

    Front-end: Add frontend container and ajax request capabilities

--- public/app.js
+++ public/app.js
@@ -17,26 +17,26 @@ app.client = {};
 // Interface for making API calls
 app.client.request = function (headers, path, method, queryStringObject, payload, callback) {
     // Set defaults
-    headers = typeof(headers) == 'object' && headers !== null ? headers : {};
-    path = typeof(path) == 'string'  ? path : '/';
-    method = typeof(method) == 'string' 
-    && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(method) > -1
-    ? method.toUpperCase() : 'GET';
-    queryStringObject = typeof(queryStringObject) == 'object' 
-    && queryStringObject !== null 
-    ? queryStringObject : {};
-    payload = typeof(payload) == 'object' && payload !== null ? payload : {};
-    callback = typeof(callback) == 'function' ? callback : false;
+    headers = typeof (headers) == 'object' && headers !== null ? headers : {};
+    path = typeof (path) == 'string' ? path : '/';
+    method = typeof (method) == 'string'
+        && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(method) > -1
+        ? method.toUpperCase() : 'GET';
+    queryStringObject = typeof (queryStringObject) == 'object'
+        && queryStringObject !== null
+        ? queryStringObject : {};
+    payload = typeof (payload) == 'object' && payload !== null ? payload : {};
+    callback = typeof (callback) == 'function' ? callback : false;
 
     // For each querystr param sent, add it to the path
     var requestUrl = path + '?';
     var counter = 0;
-    for(var queryKey in queryStringObject){
-        if(queryStringObject.hasOwnProperty(queryKey)){
+    for (var queryKey in queryStringObject) {
+        if (queryStringObject.hasOwnProperty(queryKey)) {
             counter++;
             // If at least one query string param has already been added, prepent new...
             if (counter > 1) {
-                requestUrl+='&';
+                requestUrl += '&';
             }
             // Add the key and the value
             requestUrl = queryKey + '=' + queryStringObject[queryKey];
@@ -49,14 +49,38 @@ app.client.request = function (headers, path, method, queryStringObject, payload
     xhr.setRequestHeader("Content-Type", "application/json");
 
     // For each header sent, add it to the request
-    for(var headerKey in headers){
-        if(headers.hasOwnProperty(headerKey)){
+    for (var headerKey in headers) {
+        if (headers.hasOwnProperty(headerKey)) {
             xhr.setRequestHeader(headerKey, headers[headerKey]);
         }
     }
 
     // If there is a current session token set, and that has a header
-    
-    
+    if (app.config.sessionToken) {
+        xhr.setRequestHeader('token', app.config.sessionToken.id);
+    }
+
+    // When the request comes back,handle the response
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var statusCode = xhr.status;
+            var responseReturned = xhr.responseText;
+
+            // Callback if requested
+            if (callback) {
+                try {
+                    var parsedResponse = JSON.parse(responseReturned);
+                    callback(statusCode, parsedResponse)
+                } catch (error) {
+                    callback(statusCode, false);
+                }
+            }
+        }
+    };
+
+    // Send the payload as JSON
+    var payloadString = JSON.stringify(payload);
+    xhr.send(payloadString);
+
 
 };
